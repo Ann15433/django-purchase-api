16dcacae8bd832e79443c57d04d1a14fc72b8a59
Ann15433 <manya.lukyanchikova@yandex.com>
2026-07-21 05:10:57 +0300

    views.py

--- purchase_api/core/views.py
+++ purchase_api/core/views.py
@@ -12,7 +12,6 @@ User = get_user_model()
 
 class CreateOrderView(APIView):
     def post(self, request, *args, **kwargs):
-        # Получаем ID товара из запроса (теперь это число, например 1)
         product_id = request.data.get('product')
         quantity = request.data.get('quantity', 1)
 
@@ -22,7 +21,6 @@ class CreateOrderView(APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        # Ищем товар по ID. Если передали не число или товара нет — сработает except
         try:
             product = Product.objects.get(id=product_id)
             price = product.price
@@ -35,7 +33,6 @@ class CreateOrderView(APIView):
 
         current_user = request.user
 
-        # Если пользователь не авторизован, берем первого пользователя из базы для теста
         if not current_user or not current_user.is_authenticated:
             try:
                 current_user = User.objects.first()
